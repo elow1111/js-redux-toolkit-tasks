77ae95bc6cb3d8ddd17f03a078172c90ba7625f0
Максим <160880502+elow1111@users.noreply.github.com>
2024-10-25 17:03:17 +0300

    usersSlice.js

--- 7/src/slices/usersSlice.js
+++ 7/src/slices/usersSlice.js
@@ -1,5 +1,21 @@
 import { createSlice, createEntityAdapter } from "@reduxjs/toolkit";
 
 // BEGIN (write your solution here)
+const usersAdapter = createEntityAdapter();
 
+const initialState = usersAdapter.getInitialState();
+const usersSlice = createSlice({
+  name: 'users',
+  initialState,
+  reducers: {
+    addUser: usersAdapter.addOne,
+    addUsers: usersAdapter.addMany,
+  },
+});
+export const { addUser, addUsers } = usersSlice.actions;
+export const {
+  selectAll: selectAllUsers,
+  selectById: selectUserById,
+} = usersAdapter.getSelectors((state) => state.users);
+export default usersSlice.reducer;
 // END
