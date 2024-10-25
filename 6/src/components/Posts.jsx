1ce227ff3c572619cd108e695c9bfb32d61670c1
Максим <160880502+elow1111@users.noreply.github.com>
2024-10-25 16:59:24 +0300

    Posts.jsx

--- 6/src/components/Posts.jsx
+++ 6/src/components/Posts.jsx
@@ -5,7 +5,16 @@ import Post from './Post.jsx';
 
 const Posts = () => {
   // BEGIN (write your solution here)
+  const postIds = useSelector((state) => state.posts.postIds);
+  const posts = useSelector((state) => state.posts.entities);
 
+  return (
+    <div>
+      {postIds.map(postId => (
+        <Post key={postId} post={posts[postId]} />
+      ))}
+    </div>
+  );
   // END
 };
 
