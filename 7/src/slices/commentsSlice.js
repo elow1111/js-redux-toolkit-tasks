ca452b28eaaa924462c7036aec8c24748273566a
Максим <160880502+elow1111@users.noreply.github.com>
2024-10-25 17:01:43 +0300

    commentsSlice.js

--- 7/src/slices/commentsSlice.js
+++ 7/src/slices/commentsSlice.js
@@ -1,5 +1,20 @@
 import { createSlice, createEntityAdapter } from "@reduxjs/toolkit";
 
 // BEGIN (write your solution here)
-
+const commentsAdapter = createEntityAdapter();
+const initialState = commentsAdapter.getInitialState();
+const commentsSlice = createSlice({
+  name: 'comments',
+  initialState,
+  reducers: {
+    addComment: commentsAdapter.addOne,
+    addComments: commentsAdapter.addMany,
+  },
+});
+export const { addComment, addComments } = commentsSlice.actions;
+export const {
+  selectAll: selectAllComments,
+  selectById: selectCommentById,
+} = commentsAdapter.getSelectors((state) => state.comments);
+export default commentsSlice.reducer;
 // END
