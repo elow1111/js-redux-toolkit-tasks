16866818923cfca4896854eb07774fc421441117
Максим <160880502+elow1111@users.noreply.github.com>
2025-01-14 13:30:16 +0300

    index.js

--- 10/src/services/index.js
+++ 10/src/services/index.js
@@ -1,13 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { tasksApi } from './tasksApi.js';
 
 // BEGIN (write your solution here)
-import tasksApi from './tasksApi';
 const store = configureStore({
-  reducer: {
-    [tasksApi.reducerPath]: tasksApi.reducer,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(tasksApi.middleware),
+    reducer: {
+        [tasksApi.reducerPath]: tasksApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(tasksApi.middleware),
 });
+
 export default store;
+
 // END
