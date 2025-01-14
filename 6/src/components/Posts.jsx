fd898edf7d92d4525780caf6a6de6aadb70c01aa
Максим <160880502+elow1111@users.noreply.github.com>
2025-01-14 13:34:18 +0300

    Posts.jsx

--- 6/src/components/Posts.jsx
+++ 6/src/components/Posts.jsx
@@ -4,18 +4,15 @@ import { useSelector } from 'react-redux';
 import Post from './Post.jsx';
 
 const Posts = () => {
-  // BEGIN (write your solution here)
-  const postIds = useSelector((state) => state.posts.postIds);
-  const posts = useSelector((state) => state.posts.entities);
+  const { ids, entities } = useSelector((state) => state.postsReducer);
 
   return (
-    <div>
-      {postIds.map(postId => (
-        <Post key={postId} post={posts[postId]} />
+    <div className="mt-3">
+      {ids.map((id) => (
+        <Post key={id} post={entities[id]} />
       ))}
     </div>
   );
-  // END
 };
 
 export default Posts;
